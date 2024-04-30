0190b17460d5012216b886d86be4fe86b55e980d
Gaëtan H <gaetanhus@gmail.com>
2024-04-30 22:45:16 +0200

    Update component.ts to include NPComponent and NPAsyncComponent types

--- src/lib/components/utils/component/component.ts
+++ src/lib/components/utils/component/component.ts
@@ -3,4 +3,7 @@ import type { Dispatch, ReactElement, SetStateAction } from "react";
 export type Component<Props> = (props: Props) => ReactElement;
 export type AsyncComponent<Props> = (props: Props) => Promise<ReactElement>;
 
+export type NPComponent<> = () => ReactElement;
+export type NPAsyncComponent<> = () => Promise<ReactElement>;
+
 export type SetState<T> = Dispatch<SetStateAction<T>>;
