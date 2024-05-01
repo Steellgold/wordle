c1cd7c5a6b3af5498b5411c6bef0cd97aa77ec64
Gaëtan H <gaetanhus@gmail.com>
2024-05-01 22:15:03 +0200

    Add new Board component for displaying game board and handle abandon functionality

---
+++ src/app/[gameId]/lib/abandon.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { auth } from "@/auth";
+import { db } from "@/lib/utils/prisma";
+
+export const handleAbandon = async({ gameId }: { gameId: string }) => {
+  "use server";
+
+  const session = await auth();
+  if (!session) return;
+
+  const party = await db.game.findUnique({
+    where: { id: gameId, userId: session.user?.id ?? "" }
+  });
+
+  if (!party) return;
+
+  await db.game.update({
+    where: { id: gameId },
+    data: { result: "WORDLE_ABORT" }
+  });
+};
---
+++ src/app/[gameId]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Component } from "@/lib/components/utils/component";
+import { handleAbandon } from "./lib/abandon";
+import { useState } from "react";
+
+type BoardProps = {
+  params: {
+    gameId: string;
+  };
+};
+
+export const Board: Component<BoardProps> = ({ params }) => {
+  const [isLive, setIsLive] = useState(true);
+
+  return (
+    <div>
+      <h1>Board</h1>
+      <p>Party ID: {params.gameId}</p>
+      <p>Is live: {isLive ? "Yes" : "No"}</p>
+
+      <form action={async() => {
+        await handleAbandon({ gameId: params.gameId })
+          .then(() => setIsLive(false));
+      }}>
+        <button type="submit" className="bg-red-400">click to Abandon</button>
+      </form>
+    </div>
+  );
+};
+
+export default Board;
