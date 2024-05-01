fb5fef9695bdc0a0f54e80801b86a58366f6a7f4
Gaëtan H <gaetanhus@gmail.com>
2024-05-01 17:57:55 +0200

    Add Discord provider to NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,14 @@
 import { env } from "@/lib/env.mjs"
 import NextAuth from "next-auth"
 import GitHub from "@auth/core/providers/github"
+import Discord from "@auth/core/providers/discord"
 import { db } from "./lib/utils/prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { auth, handlers } = NextAuth({
   providers: [
-    GitHub({
-      clientId: env.AUTH_GITHUB_CLIENT_ID,
-      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET
-    })
+    GitHub,
+    Discord
   ],
   adapter: PrismaAdapter(db)
 })
