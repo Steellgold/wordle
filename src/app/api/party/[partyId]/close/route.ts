b14512761ed40a3a5ad638bf5e5b594a8bd813b5
Gaëtan H <gaetanhus@gmail.com>
2024-05-02 22:48:40 +0200

    Add new route for closing a party by partyId

---
+++ src/app/api/party/[partyId]/close/route.ts
@@ -0,0 +1,32 @@
+import { auth } from "@/auth";
+import { env } from "@/lib/env.mjs";
+import { dayJS } from "@/lib/utils/dayjs/day-js";
+import { db } from "@/lib/utils/prisma";
+import { GAME_RESULT } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+type Party = {
+  params: {
+    partyId: string;
+  };
+};
+
+export const POST = async (req: NextRequest, { params }: Party) => {
+  const session = await auth();
+  if (!session) return NextResponse.redirect(env.NEXT_PUBLIC_APP_URL);
+
+  const { partyId } = params;
+
+  const party = await db.game.findUnique({ where: { id: partyId, userId: session.user?.id ?? "" } });
+  if (!party) return NextResponse.redirect(env.NEXT_PUBLIC_APP_URL);
+
+  const schema = z.object({ result: z.nativeEnum(GAME_RESULT) }).safeParse(await req.json());
+
+  await db.game.update({
+    where: { id: partyId },
+    data: { result: schema.data?.result, isLive: false, endedAt: dayJS().toISOString() }
+  });
+
+  return NextResponse.json({ ...party });
+}
