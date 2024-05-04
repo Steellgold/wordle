52ed58e301765934967db145a1e00dc824a2c753
Gaëtan H <gaetanhus@gmail.com>
2024-05-04 20:42:38 +0200

    Add utility functions for minimizing and adding zeros

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs));
 };
+
+// https://gist.github.com/Steellgold/084124512869d82f9b3a5ff0ab8adbba
+export const minimize = (wLarge: string, wMobile: string, width: number) => {
+  return width < 640 ? wMobile : wLarge;
+}
+
+// https://gist.github.com/Steellgold/de61bda61e10233fe0bfae5968bebe12
+export const addZero = (i: number) => {
+  return i < 10 ? `0${i}` : i;
+}
