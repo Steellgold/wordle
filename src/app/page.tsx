5327a32e49a75c2c3e75224be28fe7cc6885ec8c
Gaëtan H <gaetanhus@gmail.com>
2024-05-05 14:27:10 +0200

    Update Wordle component to handle guest users and display user information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,29 @@ import { ReactElement } from "react";
 import { auth } from "../auth";
 import { WordleLayout } from "@/components/wordle/layout";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
-import { User } from "lucide-react";
+import { User as IconUser } from "lucide-react";
 import { Separator } from "@/ui/separator";
 import Image from "next/image";
 import { HomeNormalStats } from "@/components/wordle/cards/stats/normal.stats";
 import { HomeSettings } from "@/lib/components/wordle/cards/settings/settings";
+import { cookies } from "next/headers";
+import { User } from "@prisma/client";
+import { db } from "@/lib/utils/prisma";
 
 const Wordle = async(): Promise<ReactElement> => {
   const session = await auth();
+  let userId: string | undefined = "";
+  if (session) userId = session.user?.id ?? "";
+  if (!session) userId = `guest${cookies().get("guestUserId")?.value}`;
+
+  let guest: User | null = null;
+  if (userId.startsWith("guest")) {
+    guest = await db.user.findUnique({
+      where: {
+        id: userId
+      }
+    })
+  }
 
   return (
     <WordleLayout>
@@ -25,7 +40,7 @@ const Wordle = async(): Promise<ReactElement> => {
                 {session?.user?.image ? (
                   <Image src={session?.user?.image} alt={session?.user?.name ?? "Username"} width={24} height={24} className="rounded-full" />
                 ) : (
-                  <User size={16} />
+                  <IconUser size={16} />
                 )}
 
                 &nbsp;
@@ -33,8 +48,13 @@ const Wordle = async(): Promise<ReactElement> => {
               </>
             ) : (
               <>
-                <User size={16} />&nbsp;
-                Guest
+                {guest?.image ? (
+                  <Image src={guest?.image} alt={"Guest Profile Picture"} width={20} height={20} className="rounded-full" />
+                ) : (
+                  <IconUser size={16} />
+                )}
+                &nbsp;
+                &nbsp;Guest
               </>
             )}
           </TabsTrigger>
